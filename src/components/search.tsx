035dd524800119fc15cafc799cf7c1b850886d28
Eustus Mwirigi <mwirigieustus97@gmail.com>
2024-07-22 16:51:46 +0300

    chore:Create basic page structure

---
+++ src/components/search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <h1>Search</h1>
+      <p>Search high-resolution images from Unsplash</p>
+    </div>
+  );
+};
+
+export default Search;
---
+++ src/pages/imageDetails.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const imageDetails = () => {
+  return (
+    <div>
+      <div>
+        <h1>Image Details</h1>
+      </div>
+      <div>details</div>
+    </div>
+  );
+};
+
+export default imageDetails;
